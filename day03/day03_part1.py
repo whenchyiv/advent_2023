e9733c1e188060fb1f85dd15bb4c9ae4ce152cfb
Will <will.henchy@gmail.com>
2023-12-08 00:13:16 -0500

    Day 03 part 1

---
+++ day03/day03_part1.py
@@ -0,0 +1,35 @@
+from input import puzzle_input
+from loguru import logger
+import re
+
+
+input_list = puzzle_input.split('\n')
+
+
+def is_special_char(val: str) -> bool:
+    return re.match(r'[^a-zA-Z0-9\s!.]', val) is not None
+
+
+if __name__ == '__main__':
+    part_numbers = []
+    # Yuck.
+    for i, r in enumerate(input_list[:]):
+        numbers = re.findall(r'(\d+)', r)
+        for n in numbers:
+            num_start = r.index(n)
+            search_start = -1 if num_start > 0 else 0
+            num_end = num_start + len(n)
+            search_end = 1 if num_end + 1 < len(r) else 0
+            search_space = r[num_start+search_start:num_end+search_end]
+            # Zero out searched number while maintaining string legnth to handle duplicate numbers in one row
+            # (.index() only gets the first index).
+            r = r.replace(r[num_start:num_end], ''.join('.'*len(n)), 1)
+            if i > 0:
+                search_space = input_list[i-1][num_start+search_start:num_end+search_end] + ' ' + search_space
+            if i + 1 < len(input_list):
+                search_space += ' ' + input_list[i+1][num_start+search_start:num_end+search_end]
+            for c in search_space:
+                if is_special_char(c):
+                    part_numbers.append(int(n))
+                    break
+    logger.info(f'⚙️ Part numbers sum: {sum(part_numbers)}')
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Advent of Code 2023
+# Advent of Regex 2023
 
 
               🎄
